11a543558c6b542c8726da3b1d0bdcb31b24e6ef
relyativus <relyativus@gmail.com>
2017-08-06 13:20:08 +0300

    Remove illegal setters from Passport

--- src/main/java/co/inventorsoft/oop/basic/model/Passport.java
+++ src/main/java/co/inventorsoft/oop/basic/model/Passport.java
@@ -15,40 +15,40 @@ public class Passport {
 
     private String address;
 
-    public String getFirstName() {
-        return firstName;
+    public Passport(final String firstName,
+                    final String lastName,
+                    final LocalDateTime dateOfBirth,
+                    final BufferedImage photo,
+                    final String address) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+        this.photo = photo;
+        this.address = address;
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
+    public String getFirstName() {
+        return firstName;
     }
 
     public String getLastName() {
         return lastName;
     }
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
     public LocalDateTime getDateOfBirth() {
         return dateOfBirth;
-    }
-
-    public void setDateOfBirth(LocalDateTime dateOfBirth) {
-        this.dateOfBirth = dateOfBirth;
     }
 
     public BufferedImage getPhoto() {
         return photo;
     }
 
-    public void setPhoto(BufferedImage photo) {
-        this.photo = photo;
-    }
-
     public String getAddress() {
         return address;
+    }
+
+    public void setPhoto(BufferedImage photo) {
+        this.photo = photo;
     }
 
     public void setAddress(String address) {
