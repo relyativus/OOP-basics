9b5d910a30ef242703605238f9ce21c6f8495253
relyativus <relyativus@gmail.com>
2017-08-06 12:59:20 +0300

    Added constructor with required attributes to Person class to avoid half-initialized classes

--- src/main/java/co/inventorsoft/oop/basic/demo/Main.java
+++ src/main/java/co/inventorsoft/oop/basic/demo/Main.java
@@ -6,7 +6,9 @@ import co.inventorsoft.oop.basic.model.Person;
 public class Main {
 
     public static void main(String[] args) {
-        final Person person = new Person(); //first problem: we can use this class before we have fully initialize it
+        final Person person = new Person(); //first problem solved: we cannot create class without specifying required
+
+        final Person martynBorodavka = new Person("Martyn", "Borodavka", 218); //much better but again we can broke everything with setters
 
         person.setAge(-2); //we can initialize it with invalid values
         person.setPassport(new Passport()); //we can create objects we are not responsible to create
--- src/main/java/co/inventorsoft/oop/basic/model/Person.java
+++ src/main/java/co/inventorsoft/oop/basic/model/Person.java
@@ -10,6 +10,12 @@ public class Person {
 
     private int age;
 
+    public Person(final String firstName, final String lastName, final int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
     public String getFirstName() {
         return firstName;
     }
